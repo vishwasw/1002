c52d3cf37296b35d14026006318cc96ad371c1cc
Jason Bell <jason.bell@treeloop.com>
2017-10-03 08:46:47 -0700

    Intro example

---
+++ Playground/Intro/Intro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person emp = new Employee();
+            emp.FirstName = "Bill";
+            emp.LastName = "Gates";
+
+            Console.WriteLine(emp.FullName);
+
+            Console.ReadKey();
+        }
+    }   
+}
+
+
